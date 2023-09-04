85850db7a001fd6cad38b6c5fdad8ed62574ffae
Javier Villalba <javiervillalba.04@gmail.com>
2023-09-04 02:22:40 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+
+const app = express();
+
+const path = require('path');
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.listen(process.env.PORT || 3000, () =>{
+    console.log("Serving on port 3000!   http://localhost:3000/");
+})
+
+app.get('/', (req,res) =>{
+    res.sendFile(path.join(__dirname, '/views/home.html'));
+})
+
+app.get('/login', (req,res) =>{
+    res.sendFile(path.join(__dirname, '/views/login.html'));
+})
+
+app.get('/register', (req,res) =>{
+    res.sendFile(path.join(__dirname, '/views/register.html'));
+})
