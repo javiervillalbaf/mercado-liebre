8a3d2cd29e9f8317cefdb9872ddaf1f7789a8b5c
Javier Villalba <javiervillalba.04@gmail.com>
2023-09-09 17:35:13 -0300

    fix

--- app.js
+++ app.js
@@ -1,23 +1,25 @@
-const express = require('express');
+const express = require("express");
 
 const app = express();
 
-const path = require('path');
+const path = require("path");
+
+const PORT = process.env.PORT || 3001;
 
 app.use(express.static(path.join(__dirname, "public")));
 
-app.listen(process.env.PORT || 3000, () =>{
-    console.log("Serving on port 3000!   http://localhost:3000/");
-})
+app.listen(PORT, () => {
+  console.log(`Servidor escuchando en puerto ${PORT}`);
+});
 
-app.get('/', (req,res) =>{
-    res.sendFile(path.join(__dirname, '/views/home.html'));
-})
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "/views/home.html"));
+});
 
-app.get('/login', (req,res) =>{
-    res.sendFile(path.join(__dirname, '/views/login.html'));
-})
+app.get("/login", (req, res) => {
+  res.sendFile(path.join(__dirname, "/views/login.html"));
+});
 
-app.get('/register', (req,res) =>{
-    res.sendFile(path.join(__dirname, '/views/register.html'));
-})
+app.get("/register", (req, res) => {
+  res.sendFile(path.join(__dirname, "/views/register.html"));
+});
